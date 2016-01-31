766cd138006ab356f44505cb3b6395f55ebb441b
Tommaso Checchi <tommaso.checchi1@gmail.com>
2016-01-31 01:46:09 +0100

    it's not clear setting which of these variables fixed it.

--- include/Dojo3D/Wheel.cpp
+++ include/Dojo3D/Wheel.cpp
@@ -9,7 +9,7 @@ void Wheel::_init() {
 	DEBUG_ASSERT(vehicle, "The parent must have created the Vehicle at this point");
 
 	index = vehicle->getNumWheels();
-	vehicle->addWheel(
+	auto& wheel = vehicle->addWheel(
 		asBtVector(connectionPoint),
 		asBtVector(direction),
 		asBtVector(axle),
@@ -18,4 +18,18 @@ void Wheel::_init() {
 		suspensionTuning,
 		true //does this do anything at all?
 	);
+
+	//TODO wtf is this stuff and why does it work with this numbers???
+
+	float	wheelFriction = 1000;//BT_LARGE_FLOAT;
+	float	suspensionStiffness = 20.f;
+	float	suspensionDamping = 2.3f;
+	float	suspensionCompression = 4.4f;
+	float	rollInfluence = 0.1f;//1.0f;
+
+	wheel.m_suspensionStiffness = suspensionStiffness;
+	wheel.m_wheelsDampingRelaxation = suspensionDamping;
+	wheel.m_wheelsDampingCompression = suspensionCompression;
+	wheel.m_frictionSlip = wheelFriction;
+	wheel.m_rollInfluence = rollInfluence;
 }
