f02256fe26e267027202466138a692a36b0291df
Tommaso Checchi <tommaso.checchi1@gmail.com>
2016-02-01 13:30:01 +0100

    scanning the mesh & discovering attributes

--- obj_to_mesh/src/main.rs
+++ obj_to_mesh/src/main.rs
@@ -2,6 +2,95 @@ extern crate wavefront_obj;
 
 use std::io::prelude::*;
 use std::fs::File;
+use wavefront_obj::obj::{ObjSet, Object, Shape, VTNIndex};
+use std::mem::size_of;
+
+#[derive(Clone, Copy)]
+enum Attribute {
+	Position,
+	Normal,
+	Tex0,
+}
+
+#[derive(Debug)]
+struct VertexFieldOffsets {
+	normal: Option<usize>,
+	tex0: Option<usize>,
+}
+
+fn has_attribute(vtni: VTNIndex, attr: Attribute) -> bool {
+	let (_, tex, normal) = vtni;
+	match attr {
+		Attribute::Position => true,
+		Attribute::Normal => normal.is_some(),
+		Attribute::Tex0 => tex.is_some()
+	}
+}
+
+fn has_all(obj: &Object, attr: Attribute) -> bool {
+
+	for geo in &obj.geometry {
+		for shape in &geo.shapes {
+			match *shape {
+				Shape::Triangle(v1, v2, v3) => {
+					if !has_attribute(v1, attr) || !has_attribute(v2, attr) || !has_attribute(v3, attr) {
+						return false;
+					}
+				},
+				Shape::Line(v1, v2) => {
+					if !has_attribute(v1, attr) || !has_attribute(v2, attr) {
+						return false;
+					}
+				},
+				Shape::Point(v1) => {
+					if !has_attribute(v1, attr) {
+						return false;
+					}
+				},
+			}
+		}
+	}
+	true
+}
+
+fn get_offset(obj: &Object, attr: Attribute, offset: &mut usize) -> Option<usize> {
+	let orig_offs = *offset;
+	if has_all(obj, attr) {
+		*offset += size_of::<f32>() * 3; //TODO tex0
+		return Some(orig_offs);
+	}
+	None
+}
+
+impl VertexFieldOffsets {
+	fn from_object(obj: &Object) -> Self {
+		let mut offset = size_of::<f32>() * 3; //size of Position
+
+		VertexFieldOffsets {
+			normal: get_offset(obj, Attribute::Normal, &mut offset),
+			tex0: get_offset(obj, Attribute::Tex0, &mut offset),
+		}
+	}
+}
+
+fn convert_obj(obj: Object) -> Vec<u8> {
+
+	let offsets = VertexFieldOffsets::from_object(&obj);
+
+	println!("{:?}", offsets);
+
+	vec![]
+}
+
+fn convert_obj_set(set: ObjSet) -> Vec<Vec<u8>> {
+	let mut data: Vec<Vec<u8>> = vec![];
+
+	for obj in set.objects {
+		data.push(convert_obj(obj));
+	}
+
+	data
+}
 
 fn main() {
 
@@ -10,8 +99,12 @@ fn main() {
 	let mut content = String::new();
 	file.read_to_string(&mut content).unwrap();
 
-	match wavefront_obj::obj::parse(content) {
-	    Ok(obj) => println!("{:?}", obj),
-	    Err(err) => println!("{:?}", err),
+	let data = match wavefront_obj::obj::parse(content) {
+	    Ok(obj) => convert_obj_set(obj),
+	    Err(err) => panic!("{:?}", err),
+	};
+
+	for mesh in data {
+		//write out
 	}
 }
